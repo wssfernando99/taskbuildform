396fe30e2c40167877fc337abef23833a4c9ce90
wssfernando99 <sarindusauranga@gmail.com>
2024-06-15 03:12:59 +0530

    Create the header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './header.css'
+
+export const Header = () => {
+  return (
+    <nav className='container'>
+        <div className='logo-container'>
+            <div className='div-1'></div>
+            <div className='div-2'></div>
+            <div className='div-3'></div>
+        </div>
+        <div className='heading-container'><h2>Buildform</h2></div>
+    </nav>
+  )
+}
